b1cf4000cd25eb99fc57637055621d070e256d67
Autostrafe <Levibautista360@gmail.com>
2024-03-19 11:48:38 -0600

    Class Practice v1
    - Created 5 print statements
    - Created Comments

--- Class Practice/src/App.java
+++ Class Practice/src/App.java
@@ -1,5 +1,16 @@
+// Program/Assignment name: A5 Class Practice
+
+// Student name: Levi Bautista
+
+// Date: 3/19/2024
+// Program Description: Assignment to practice print statements and escape sequences
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Computer Science 30");
+        System.out.println("My Name is Levi and my favourite food is shrimp tempura.");
+        System.out.println("Java has all sorta of weird \"quirks\" which make displaying text a bit more challenging.");
+        System.out.println("Sometimes characters like parentheses (sometimes called brackets) don't work correctly.");
+        System.out.println("Escape sequences require us to use a backslash '\\' in order to output these special characters.");
     }
 }
